a9694da79cea28e420d8cbbade6da8a18da10d82
Jandoer0 <104924974+Jandoer0@users.noreply.github.com>
2024-12-09 20:45:14 +0700

    Update show_qr.sh

--- show_qr.sh
+++ show_qr.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
 
+# Проверка операционной системы
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+    echo "Этот скрипт работает только на Linux."
+    exit 1
+fi
+
+# Проверка и установка необходимых программ
+if ! command -v qrencode &> /dev/null; then
+    echo "Устанавливаем qrencode..."
+    sudo apt-get update
+    sudo apt-get install -y qrencode
+fi
+
+if ! command -v wg &> /dev/null; then
+    echo "Устанавливаем WireGuard..."
+    sudo apt-get update
+    sudo apt-get install -y wireguard
+fi
+
+# Запрос пути к файлу конфигурации клиента WireGuard
+read -p "Введите путь к файлу конфигурации клиента WireGuard: " config_path
+
+# Проверка существования файла конфигурации
+if [[ ! -f "$config_path" ]]; then
+    echo "Файл конфигурации не найден!"
+    exit 1
+fi
+
+# Отображение QR кода
+qrencode -t ansiutf8 < "$config_path"
